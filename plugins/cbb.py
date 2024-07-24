67da557247c7354e17d676c26b2a83ac4e84ee36
Adnanffx <ffxadnan25@gmail.com>
2024-07-24 20:41:03 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,13 +10,13 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ ᴏᴡɴᴇʀ : <a href=https://t.me/Whiteofz'>user?id={7432102513}'>whiteoz</a>\n○ ᴍʏ ᴜᴘᴅᴀᴛᴇs : <a href='https://t.me/Anime_Hindi_Sub_Industry'>ᴀɴɪᴍᴇ sᴜʙ ɪɴᴅᴜsᴛʀʏ</a>\n○ ᴀɴɪᴍᴇ ᴍᴏᴠɪᴇ : <a href='https://t.me/Anime_Movies_Hindi_Sub_Industry'>ᴀɴɪᴍᴇ ᴍᴏᴠɪᴇ ɪɴᴅᴜsᴛʀʏ</a>\n○ Aɴɪᴍᴇ ɪɴᴅᴇx : <a href='@Anime_Hindi_Sub_Industry_Index'>Aɴɪᴍᴇ ɪɴᴅᴜsᴛʀʏ ɪɴᴅᴇx</a>\n○ Dᴇᴠᴇʟᴏᴘᴇᴅ Bʏ : <a href='https://t.me/i_killed_my_clan'>๏ 𝐎ʙɪᴛᴏ ᴜᴄʜɪʜᴀ ๏</a></b>",
+            text = f"<b>○ ᴏᴡɴᴇʀ : <a href='https://t.me/rin_nanakura={7432102513}'>rin nanakura</a>\n○ ᴍʏ ᴜᴘᴅᴀᴛᴇs : <a href='https://t.me/ongoing_society'>ᴏɴɢᴏɪɴɢ sᴏᴄɪᴇᴛʏ</a>\n○ ᴀɴɪᴍᴇ sᴜʙ sᴏᴄɪᴇᴛʏ : <a href='https://t.me/anime_sub_society'>ᴀɴɪᴍᴇ sᴜʙ sᴏᴄɪᴇᴛʏ</a>\n○ ᴀɴɪᴍᴇ ᴄʜᴀᴛ : <a href='https://t.me/AHSS_HELP_ZONE'>sᴏᴄɪᴇᴛʏ ᴄʜᴀᴛ ᴢᴏɴᴇ</a>\n○ Dᴇᴠᴇʟᴏᴘᴇᴅ Bʏ : <a href='https://t.me/i_killed_my_clan'>๏ 𝐎ʙɪᴛᴏ ᴜᴄʜɪʜᴀ ๏</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
                     InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data = "close"),
-                    InlineKeyboardButton('ᴘʀᴇᴍɪᴜᴍ', url='https://t.me/AHSS_HELP_ZONE')
+                    InlineKeyboardButton('⛩️ ʜᴏᴍᴇ ⛩️ ', url='https://t.me/AHSS_HELP_ZONE')
                     ]
                 ]
             )
